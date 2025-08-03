4cff7e744011edbf2e28e4dcc6a38fdef0897eca
john89000 <122229643+john89000@users.noreply.github.com>
2025-08-03 15:31:25 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,10 @@
+-- Question 1:
+-- Write an SQL query to create a new database called salesDB
+CREATE DATABASE salesDB;
+
+-- Question 2:
+/* 
+I first created the database demo, then dropped it.
+*/
+CREATE DATABASE demo;
+DROP DATABASE demo;
